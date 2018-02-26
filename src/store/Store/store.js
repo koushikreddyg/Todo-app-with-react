b05e98be24b9def18f02e239646a424722c12149
koushikui <koushikreddygunapati@gmail.com>
2018-02-25 22:29:50 -0500

    added changes after database

--- src/store/Store/store.js
+++ src/store/Store/store.js
@@ -2,13 +2,14 @@ import {createStore,combineReducers,compose,applyMiddleware} from 'redux';
 import taskActions from '../Reducer/taskActions';
 import Authreducers from '../Reducer/AuthReducer';
 import thunk from 'redux-thunk';
+const Composer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||compose
 export default()=>{
     const store=(createStore(combineReducers({
         Actions:taskActions,
         Auth: Authreducers,
     }),
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  
-    applyMiddleware(thunk)
+    Composer(applyMiddleware(thunk))
 
 )
 
